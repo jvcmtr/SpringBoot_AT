c325a6160bcc44981e64b8f0b3557c89882c0527
jvcmtr <jvictorcmtr@gmail.com>
2023-12-07 16:54:35 -0300

    first

---
+++ Joao_Ramos_DR3_AT/src/main/java/edu/SpringbootAT/Joao_Ramos_DR3_AT/JoaoRamosDr3AtApplication.java
@@ -0,0 +1,13 @@
+package edu.SpringbootAT.Joao_Ramos_DR3_AT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaoRamosDr3AtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaoRamosDr3AtApplication.class, args);
+	}
+
+}
